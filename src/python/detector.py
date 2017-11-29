aa295280b12e6f8bc2f4b388c1266d9abff30394
Atabak <atabakd@gmail.com>
2017-11-29 02:03:59 +0000

    tested detector

--- src/python/detector.py
+++ src/python/detector.py
@@ -3,26 +3,31 @@ from __future__ import print_function, absolute_import, division
 import numpy as np
 import os
 import tensorflow as tf
-#from six.moves import urllib
-from six.moves.urllib.request import urlretrieve
 import sys
 import inspect
-
+#from subprocess import call
 
 class Detector(object):
   def __init__(self, NUM_CLASSES=3):
     self.dirname = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
     self.path = os.path.join(self.dirname,'detector.py')
-    root = os.path.dirname(self.dirname)
-    self.infer_path = os.path.join(root, 'inference_data')
-    sys.path.append(os.path.join(root, 'lib', 'model'))
+    root = os.path.dirname(os.path.dirname(self.dirname))
+    infer_path = os.path.join(root, 'inference_data')
+    obj_detect_path = os.path.join(root, 'lib', 'models', 'research', 'object_detection')
+    sys.path.append(obj_detect_path)
+    sys.path.append(os.path.join(root, 'lib', 'models', 'research'))
+    sys.path.append(os.path.join(root, 'lib', 'models', 'research', 'slim'))
+    
+   # os.chdir(os.path.join(root, 'lib', 'models', 'research/'))    
+   # print(os.getcwd())
+   # call(["protoc", "object_detection/protos/*.proto", "--python_out=."])
     
     from utils import label_map_util
-    from utils import visualization_utils# as vis_util
+    from utils import visualization_utils
     self.vis_util = visualization_utils
     
-    self.graph_path = self.maybe_download()
-    self.label_path = os.path.join(self.infer_path,'label_map.pbtxt')
+    self.graph_path = os.path.join(infer_path, 'frozen_inference_graph.pb')
+    self.label_path = os.path.join(infer_path,'label_map.pbtxt')
     
     label_map = label_map_util.load_labelmap(self.label_path)
     categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
@@ -44,32 +49,6 @@ class Detector(object):
     self.num_detections = detection_graph.get_tensor_by_name('num_detections:0')
     self.sess = tf.Session(graph=detection_graph)
     
-    
-  def maybe_download(self, force=False):
-    
-    def download_progress_hook(count, blockSize, totalSize):
-      """A hook to report the progress of a download. This is mostly intended for users with
-      slow internet connections. Reports every 5% change in download progress.
-      """
-      global last_percent_reported
-      percent = int(count * blockSize * 100 / totalSize)
-    
-      if last_percent_reported != percent:
-        if percent % 5 == 0:
-          sys.stdout.write("%s%%" % percent)
-          sys.stdout.flush()
-        else:
-          sys.stdout.write(".")
-          sys.stdout.flush()
-          
-        last_percent_reported = percent
-    
-    dest_filename = os.path.join(self.infer_path, 'frozen_inference_graph.pb')
-    if force or not os.path.exists(dest_filename):
-      print('Attempting to download: frozen_inference_graph.pb')
-      link = 'https://mega.nz/#!GMBx3SID!N93BRx0Jyfy0exLd8S3lD4nW7x4VV6LlTazXPXW7gE4'
-      urlretrieve(link, dest_filename, reporthook=download_progress_hook)
-    return dest_filename
   
   def overlay_shapes(self, rgb_uint8_img):
     image_np_expanded = np.expand_dims(rgb_uint8_img, axis=0)
@@ -94,6 +73,8 @@ class Detector(object):
     
 if __name__ == "__main__":
   import skimage
+  import skimage.io
+  import skimage.transform
   from PIL import Image
   from matplotlib import pyplot as plt
   
@@ -104,7 +85,7 @@ if __name__ == "__main__":
   
   
   my_detector = Detector()
-  img_path = "/home/atabak/Desktop/box.png"
+  img_path = os.path.expanduser('~') + "/Desktop/box.png"
   image = skimage.io.imread(img_path)
   image = skimage.transform.resize(image, [300,300], mode='reflect')
   skimage.io.imsave(img_path, image)
@@ -112,3 +93,4 @@ if __name__ == "__main__":
   image_np = load_image_into_numpy_array(image)
   my_detector.overlay_shapes(image_np)
   plt.imshow(image_np)
+  plt.savefig('test2.png', format='png', bbox_inches='tight')
