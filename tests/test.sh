31519ab49859752e130cf2c174542dd577c68ffd
rui <ruihortafigueiredo@gmail.com>
2018-04-02 10:58:26 +0100

    using opencv 3.4

---
+++ tests/test.sh
@@ -0,0 +1,2 @@
+#./build/object_detector -m=/home/rui/ws/src/active_semantic_mapping/lib/shape-detection/models/frozen_inference_graph_face.pb 
+./build/object_detector -c=/home/rui/ws/src/active_semantic_mapping/lib/shape-detection/models/res10_300x300_ssd_iter_140000.caffemodel -m=/home/rui/ws/src/active_semantic_mapping/lib/shape-detection/models/deploy.prototxt.txt
