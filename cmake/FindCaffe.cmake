c625d2fc1cfee1868d6c4c0c3692e0b69bcb9d8d
ruipimentelfigueiredo <ruihortafigueiredo@gmail.com>
2018-09-04 15:41:16 +0100

    removed unnecessary dependencies

--- cmake/FindCaffe.cmake
+++ cmake/FindCaffe.cmake
@@ -4,11 +4,11 @@ unset(Caffe_FOUND)
 
 find_path(Caffe_INCLUDE_DIRS NAMES caffe/caffe.hpp caffe/common.hpp caffe/net.hpp caffe/proto/caffe.pb.h caffe/util/io.hpp
   HINTS
-  /home/rui/caffe/build/install/include/) #example: /home/jonhdoe/caffe
+  /home/vislab/caffe/build/install/include/) #example: /home/jonhdoe/caffe
 
 find_library(Caffe_LIBS NAMES caffe
   HINTS
-  /home/rui/caffe/build/lib)
+  /home/vislab/caffe/build/lib)
 
 if(Caffe_LIBS AND Caffe_INCLUDE_DIR)
     set(Caffe_FOUND 1)
